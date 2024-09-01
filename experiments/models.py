ba3cadaf9f06b3b14f75f67c18f6a4fb84506879
dariofenoglio98 <139016178+dariofenoglio98@users.noreply.github.com>
2024-09-01 23:16:57 +0200

    test training Lenet5 resnet9

--- experiments/models.py
+++ experiments/models.py
@@ -22,10 +22,31 @@ config = {
     },
     'adult': {
         'task_type': 'classification',
-        'num_classes': 1,
+        'num_classes': 2,
         'input_dim': 105,
     }
 }
+
+# define device
+def check_gpu(manual_seed=True, print_info=True):
+    if manual_seed:
+        torch.manual_seed(0)
+    if torch.cuda.is_available():
+        if print_info:
+            print("CUDA is available")
+        device = 'cuda:1'
+        torch.cuda.manual_seed_all(0) 
+    elif torch.backends.mps.is_available():
+        if print_info:
+            print("MPS is available")
+        device = torch.device("mps")
+        torch.mps.manual_seed(0)
+    else:
+        if print_info:
+            print("CUDA is not available")
+        device = 'cpu'
+    return device
+
 
 #########################################################################################
 # Linear regression model
@@ -48,8 +69,6 @@ class LinearModel(nn.Module):
         
         if dataset_name == 'airline':
             self.criterion = nn.MSELoss()    
-        elif dataset_name == 'adult':
-            self.criterion = nn.BCELoss()
         else:
             self.criterion = nn.CrossEntropyLoss()
                 
@@ -66,12 +85,8 @@ class LinearModel(nn.Module):
         x = self.output_layer(x)
         
         if self.task_type == 'classification':
-            if self.output_layer.out_features == 1:
-                # Binary classification (single output node)
-                x = torch.sigmoid(x)
-            else:
-                # Multiclass classification (multiple output nodes)
-                x = torch.softmax(x, dim=1)
+            # Multiclass classification (multiple output nodes)
+            x = torch.softmax(x, dim=1)
         
         return x
 
@@ -105,6 +120,11 @@ class MLP(nn.Module):
         input_dim = config[dataset_name]['input_dim']
         output_dim = config[dataset_name]['num_classes']
         
+        if dataset_name == 'airline':
+            self.criterion = nn.MSELoss()    
+        else:
+            self.criterion = nn.CrossEntropyLoss()
+        
         self.fc1 = nn.Linear(input_dim, hidden_dim)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(hidden_dim, hidden_dim)
@@ -126,12 +146,8 @@ class MLP(nn.Module):
         x = self.fc3(x)
         
         if self.task_type == 'classification':
-            if self.fc3.out_features == 1:
-                # Binary classification (single output node)
-                x = torch.sigmoid(x)
-            else:
-                # Multiclass classification (multiple output nodes)
-                x = torch.softmax(x, dim=1)
+            # Multiclass classification (multiple output nodes)
+            x = torch.softmax(x, dim=1)
         
         return x
 
@@ -157,6 +173,9 @@ class CNN(nn.Module):
         
         input_channels = config[dataset_name]['input_channels']
         num_classes = config[dataset_name]['num_classes']
+        
+        self.criterion = nn.CrossEntropyLoss()
+        self.task_type = 'classification'
                 
         self.conv1 = nn.Conv2d(input_channels, 16, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
@@ -195,6 +214,119 @@ class CNN(nn.Module):
 # model = CNN(input_channels=3, num_classes=10)  # For example, 3 channels (RGB) and 10 classes (e.g., CIFAR-10)
 
 
+
+class LeNet5Flexible(nn.Module):
+    def __init__(self, dataset_name):
+        """
+        Initializes a flexible CNN model inspired by LeNet-5 for classification tasks.
+
+        Args:
+            dataset_name (str): The name of the dataset, used to configure the model.
+        """
+        super(LeNet5Flexible, self).__init__()
+        
+        input_channels = config[dataset_name]['input_channels']
+        num_classes = config[dataset_name]['num_classes']
+        
+        self.criterion = nn.CrossEntropyLoss()
+        self.task_type = 'classification'
+        
+        # LeNet-5 Inspired Convolutional Layers
+        self.conv1 = nn.Conv2d(input_channels, 6, kernel_size=5, stride=1, padding=2)  # LeNet-5 uses 6 filters
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5, stride=1)  # LeNet-5 uses 16 filters
+        
+        # MaxPooling Layer
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        # Fully connected layers (will be dynamically initialized based on the input image size)
+        self.fc1 = None
+        self.fc2 = None
+        self.fc3 = None
+        self.num_classes = num_classes
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))  # First Conv + Pooling
+        x = self.pool(F.relu(self.conv2(x)))  # Second Conv + Pooling
+
+        # Flatten the feature map
+        x = x.view(x.size(0), -1)
+        
+        # Dynamically initialize fully connected layers
+        if self.fc1 is None:
+            num_features = x.size(1)
+            self.fc1 = nn.Linear(num_features, 120)  # LeNet-5 uses 120 neurons
+            self.fc2 = nn.Linear(120, 84)  # LeNet-5 uses 84 neurons
+            self.fc3 = nn.Linear(84, self.num_classes)
+            
+            # Move fully connected layers to the same device as input
+            self.fc1.to(x.device)
+            self.fc2.to(x.device)
+            self.fc3.to(x.device)
+
+        x = F.relu(self.fc1(x))  # First Fully Connected Layer
+        x = F.relu(self.fc2(x))  # Second Fully Connected Layer
+        x = self.fc3(x)  # Output Layer
+        
+        # Apply softmax for classification
+        return torch.softmax(x, dim=1)
+
+
+import torch.nn.functional as F
+
+class BasicBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+    
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+class ResNet9(nn.Module):
+    def __init__(self, dataset_name):
+        super(ResNet9, self).__init__()
+        
+        input_channels = config[dataset_name]['input_channels']
+        num_classes = config[dataset_name]['num_classes']
+        
+        self.criterion = nn.CrossEntropyLoss()
+        self.task_type = 'classification'
+        
+        self.conv1 = nn.Conv2d(input_channels, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        
+        self.layer1 = BasicBlock(64, 128, stride=2)
+        self.layer2 = BasicBlock(128, 256, stride=2)
+        self.layer3 = BasicBlock(256, 512, stride=2)
+        
+        self.pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, num_classes)
+    
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        
+        x = self.pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        
+        return x
 
 #########################################################################################
 # Transformer model
