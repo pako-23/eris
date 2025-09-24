41d5561b09f272398259da436a0e3fe5fdcae1ce
dariofenoglio98 <dario.fenoglio@usi.ch>
2025-09-24 09:57:52 +0200

    gpt codes cleaning

--- exps_GPT/run.sh
+++ exps_GPT/run.sh
@@ -7,64 +7,60 @@ local_epochs=2
 gradient_accumulation_steps=8  #8, change to 4 with 16 samples
 model_name="EleutherAI/gpt-neo-1.3B"  # or gpt2-xl, gpt2, 
 output_dir="./outputs_gpt_neo_1.3B_DP"
-# model_name="gpt2"  # or gpt2-xl, gpt2,
-# output_dir="./outputs_gpt2_test"
-# client_training_samples=(4 8 16 32 64 128)
-client_training_samples=(32 64 128)
+client_training_samples=(32 64 128) # (4 8 16 32 64 128)
 
 
-# # FedAvg
-# method="FedAvg"
-# echo -e "\n\033-- FedAvg --\033[0m"
-# echo -e "\n\033[1;36mStart training on GPT with $n_clients clients, method: $method, total number of samples: $client_training_samples, folds: $n_folds\033[0m"
 
-# for n_samples in "${client_training_samples[@]}"; do
-#     echo -e "\n\033[1;36mStarting experiment with $n_samples samples\033[0m"
+# FedAvg
+method="FedAvg"
+echo -e "\n\033-- FedAvg --\033[0m"
+echo -e "\n\033[1;36mStart training on GPT with $n_clients clients, method: $method, total number of samples: $client_training_samples, folds: $n_folds\033[0m"
 
-#     # Cycle through the folds
-#     for fold in $(seq 0 $((n_folds - 1))); do
-#         echo -e "\n\033[1;36mFold $fold - Creating dataset with $n_samples samples\033[0m"
+for n_samples in "${client_training_samples[@]}"; do
+    echo -e "\n\033[1;36mStarting experiment with $n_samples samples\033[0m"
 
-#         python local_fl_training.py --model_name $model_name --n_clients $n_clients --client_training_samples $n_samples --fold $fold --fl_rounds $fl_rounds --fold $fold --output_dir $output_dir --local_epochs $local_epochs --gradient_accumulation_steps $gradient_accumulation_steps
+    # Cycle through the folds
+    for fold in $(seq 0 $((n_folds - 1))); do
+        echo -e "\n\033[1;36mFold $fold - Creating dataset with $n_samples samples\033[0m"
 
-#     done
+        python fl_training.py --model_name $model_name --n_clients $n_clients --client_training_samples $n_samples --fold $fold --fl_rounds $fl_rounds --fold $fold --output_dir $output_dir --local_epochs $local_epochs --gradient_accumulation_steps $gradient_accumulation_steps
 
-#     # Aggregate results
-#     echo -e "\n\033[1;36mAggregating results from $n_folds folds\033[0m\n"
-#     python average_results.py --results_dir $output_dir --n_folds $n_folds --method $method --n_samples $n_samples
+    done
 
-#     echo -e "\n\033[1;36mFinished training correctly!\033[0m\n"
+    # Aggregate results
+    echo -e "\n\033[1;36mAggregating results from $n_folds folds\033[0m\n"
+    python average_results.py --results_dir $output_dir --n_folds $n_folds --method $method --n_samples $n_samples
 
-# done
+    echo -e "\n\033[1;36mFinished training correctly!\033[0m\n"
+
+done
 
 
 
 
-# # ERIS
-# method="ERIS"
-# echo -e "\n\033-- ERIS --\033[0m"
-# echo -e "\n\033[1;36mStart training on GPT with $n_clients clients, method: $method, total number of samples: $client_training_samples, folds: $n_folds\033[0m"
+# ERIS
+method="ERIS"
+echo -e "\n\033-- ERIS --\033[0m"
+echo -e "\n\033[1;36mStart training on GPT with $n_clients clients, method: $method, total number of samples: $client_training_samples, folds: $n_folds\033[0m"
 
-# for n_samples in "${client_training_samples[@]}"; do
-#     echo -e "\n\033[1;36mStarting experiment with $n_samples samples\033[0m"
+for n_samples in "${client_training_samples[@]}"; do
+    echo -e "\n\033[1;36mStarting experiment with $n_samples samples\033[0m"
 
-#     # Cycle through the folds
-#     for fold in $(seq 0 $((n_folds - 1))); do
-#         echo -e "\n\033[1;36mFold $fold - Creating dataset with $n_samples samples\033[0m"
+    # Cycle through the folds
+    for fold in $(seq 0 $((n_folds - 1))); do
+        echo -e "\n\033[1;36mFold $fold - Creating dataset with $n_samples samples\033[0m"
 
-#         python local_fl_training_eris.py --model_name $model_name --n_clients $n_clients --client_training_samples $n_samples --fold $fold --fl_rounds $fl_rounds --fold $fold --output_dir $output_dir --local_epochs $local_epochs --gradient_accumulation_steps $gradient_accumulation_steps
+        python fl_training_eris.py --model_name $model_name --n_clients $n_clients --client_training_samples $n_samples --fold $fold --fl_rounds $fl_rounds --fold $fold --output_dir $output_dir --local_epochs $local_epochs --gradient_accumulation_steps $gradient_accumulation_steps
 
-#     done
+    done
 
-#     # Aggregate results
-#     echo -e "\n\033[1;36mAggregating results from $n_folds folds\033[0m\n"
-#     python average_results.py --results_dir $output_dir --n_folds $n_folds --method $method --n_samples $n_samples
+    # Aggregate results
+    echo -e "\n\033[1;36mAggregating results from $n_folds folds\033[0m\n"
+    python average_results.py --results_dir $output_dir --n_folds $n_folds --method $method --n_samples $n_samples
 
-#     echo -e "\n\033[1;36mFinished training correctly!\033[0m\n"
+    echo -e "\n\033[1;36mFinished training correctly!\033[0m\n"
 
-# done
-
-
+done
 
 
 
@@ -81,7 +77,7 @@ for n_samples in "${client_training_samples[@]}"; do
     for fold in $(seq 0 $((n_folds - 1))); do
         echo -e "\n\033[1;36mFold $fold - Creating dataset with $n_samples samples\033[0m"
 
-        python local_fl_training_dp.py --model_name $model_name --n_clients $n_clients --client_training_samples $n_samples --fold $fold --fl_rounds $fl_rounds --fold $fold --output_dir $output_dir --local_epochs $local_epochs --gradient_accumulation_steps $gradient_accumulation_steps
+        python fl_training_dp.py --model_name $model_name --n_clients $n_clients --client_training_samples $n_samples --fold $fold --fl_rounds $fl_rounds --fold $fold --output_dir $output_dir --local_epochs $local_epochs --gradient_accumulation_steps $gradient_accumulation_steps
 
     done
 
@@ -96,57 +92,78 @@ done
 
 
 
+# PriPrune
+method="PriPrune"
+echo -e "\n\033-- PriPrune --\033[0m"
+echo -e "\n\033[1;36mStart training on GPT with $n_clients clients, method: $method, total number of samples: $client_training_samples, folds: $n_folds\033[0m"
 
+for n_samples in "${client_training_samples[@]}"; do
+    echo -e "\n\033[1;36mStarting experiment with $n_samples samples\033[0m"
 
-# # PriPrune
-# method="PriPrune"
-# echo -e "\n\033-- PriPrune --\033[0m"
-# echo -e "\n\033[1;36mStart training on GPT with $n_clients clients, method: $method, total number of samples: $client_training_samples, folds: $n_folds\033[0m"
+    # Cycle through the folds
+    for fold in $(seq 0 $((n_folds - 1))); do
+        echo -e "\n\033[1;36mFold $fold - Creating dataset with $n_samples samples\033[0m"
 
-# for n_samples in "${client_training_samples[@]}"; do
-#     echo -e "\n\033[1;36mStarting experiment with $n_samples samples\033[0m"
+        python fl_training_pruning.py --model_name $model_name --n_clients $n_clients --client_training_samples $n_samples --fold $fold --fl_rounds $fl_rounds --fold $fold --output_dir $output_dir --local_epochs $local_epochs --gradient_accumulation_steps $gradient_accumulation_steps
 
-#     # Cycle through the folds
-#     for fold in $(seq 0 $((n_folds - 1))); do
-#         echo -e "\n\033[1;36mFold $fold - Creating dataset with $n_samples samples\033[0m"
+    done
 
-#         python local_fl_training_pruning.py --model_name $model_name --n_clients $n_clients --client_training_samples $n_samples --fold $fold --fl_rounds $fl_rounds --fold $fold --output_dir $output_dir --local_epochs $local_epochs --gradient_accumulation_steps $gradient_accumulation_steps
+    # Aggregate results
+    echo -e "\n\033[1;36mAggregating results from $n_folds folds\033[0m\n"
+    python average_results.py --results_dir $output_dir --n_folds $n_folds --method $method --n_samples $n_samples
 
-#     done
+    echo -e "\n\033[1;36mFinished training correctly!\033[0m\n"
 
-#     # Aggregate results
-#     echo -e "\n\033[1;36mAggregating results from $n_folds folds\033[0m\n"
-#     python average_results.py --results_dir $output_dir --n_folds $n_folds --method $method --n_samples $n_samples
-
-#     echo -e "\n\033[1;36mFinished training correctly!\033[0m\n"
-
-# done
-
+done
 
 
 
+# SoteriaFL
+method="SoteriaFL"
+echo -e "\n\033-- SoteriaFL --\033[0m"
+echo -e "\n\033[1;36mStart training on GPT with $n_clients clients, method: $method, total number of samples: $client_training_samples, folds: $n_folds\033[0m"
+
+for n_samples in "${client_training_samples[@]}"; do
+    echo -e "\n\033[1;36mStarting experiment with $n_samples samples\033[0m"
+
+    # Cycle through the folds
+    for fold in $(seq 0 $((n_folds - 1))); do
+        echo -e "\n\033[1;36mFold $fold - Creating dataset with $n_samples samples\033[0m"
+
+        python fl_training_soteriafl.py --model_name $model_name --n_clients $n_clients --client_training_samples $n_samples --fold $fold --fl_rounds $fl_rounds --fold $fold --output_dir $output_dir --local_epochs $local_epochs --gradient_accumulation_steps $gradient_accumulation_steps
+
+    done
+
+    # Aggregate results
+    echo -e "\n\033[1;36mAggregating results from $n_folds folds\033[0m\n"
+    python average_results.py --results_dir $output_dir --n_folds $n_folds --method $method --n_samples $n_samples
+
+    echo -e "\n\033[1;36mFinished training correctly!\033[0m\n"
+
+done
 
 
-# # SoteriaFL
-# method="SoteriaFL"
-# echo -e "\n\033-- SoteriaFL --\033[0m"
-# echo -e "\n\033[1;36mStart training on GPT with $n_clients clients, method: $method, total number of samples: $client_training_samples, folds: $n_folds\033[0m"
 
-# for n_samples in "${client_training_samples[@]}"; do
-#     echo -e "\n\033[1;36mStarting experiment with $n_samples samples\033[0m"
+# Shatter
+method="Shatter"
+echo -e "\n\033[1;36m-- Shatter --\033[0m"
+echo -e "\n\033[1;36mStart training on GPT with $n_clients clients, method: $method, total number of samples: $client_training_samples, folds: $n_folds\033[0m"
 
-#     # Cycle through the folds
-#     for fold in $(seq 0 $((n_folds - 1))); do
-#         echo -e "\n\033[1;36mFold $fold - Creating dataset with $n_samples samples\033[0m"
+for n_samples in "${client_training_samples[@]}"; do
+    echo -e "\n\033[1;36mStarting experiment with $n_samples samples\033[0m"
 
-#         python local_fl_training_soteriafl.py --model_name $model_name --n_clients $n_clients --client_training_samples $n_samples --fold $fold --fl_rounds $fl_rounds --fold $fold --output_dir $output_dir --local_epochs $local_epochs --gradient_accumulation_steps $gradient_accumulation_steps
+    # Cycle through the folds
+    for fold in $(seq 0 $((n_folds - 1))); do
+        echo -e "\n\033[1;36mFold $fold - Creating dataset with $n_samples samples\033[0m"
 
-#     done
+        python fl_training_shatter.py --model_name $model_name --n_clients $n_clients --client_training_samples $n_samples --fold $fold --fl_rounds $fl_rounds --fold $fold --output_dir $output_dir --local_epochs $local_epochs --gradient_accumulation_steps $gradient_accumulation_steps
 
-#     # Aggregate results
-#     echo -e "\n\033[1;36mAggregating results from $n_folds folds\033[0m\n"
-#     python average_results.py --results_dir $output_dir --n_folds $n_folds --method $method --n_samples $n_samples
+    done
 
-#     echo -e "\n\033[1;36mFinished training correctly!\033[0m\n"
+    # Aggregate results
+    echo -e "\n\033[1;36mAggregating results from $n_folds folds\033[0m\n"
+    python average_results.py --results_dir $output_dir --n_folds $n_folds --method $method --n_samples $n_samples
 
-# done
+    echo -e "\n\033[1;36mFinished training correctly!\033[0m\n"
+
+done
